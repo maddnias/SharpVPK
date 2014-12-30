7bbb0550192e592005d580cb7b00605378a67245
UbbeLoL <ubbelolhf@gmail.com>
2014-12-30 12:46:42 +0100

    initial release

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-SharpVPK
+SharpVPK:
 ========
 
-A parser for Valve's VPK files created in C#
+A memory-friendly and fast parser for Valve's VPK files created in C#
+========
+
+Example usage
+
+```cs
+    var archive = new VpkArchive();
+    archive.Load(@"vpk_file_path.vpk");
+    
+    foreach(var directory in archive.Directories)
+    	foreach (var entry in directory.Entries)
+    		Console.WriteLine(entry.ToString());
+    
+    Console.WriteLine("Done");
+    Console.ReadLine();
+```
---
+++ SharpVPK.Test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpVPK.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var archive = new VpkArchive();
+            archive.Load(@"G:\Steam\SteamApps\common\dota 2 beta\dota\pak01_dir.vpk");
+
+            //foreach(var dir in archive.Directories)
+            //    foreach(var entry in dir.Entries)
+            //        if (entry.HasPreloadData)
+            //        {
+ 
+            //        }
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
